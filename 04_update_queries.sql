8df777dbb977ec1ed57f60df5f84547efdf7addf
Senzo21 <luthernakumba2000@gmail.com>
2025-09-22 03:44:00 +0200

    feat(update): add script to update patron membership status

--- 04_update_queries.sql
+++ 04_update_queries.sql
@@ -9,4 +9,4 @@ UPDATE patrons SET borrowed_books = array_append(borrowed_books, 1) WHERE id = 1
 
 -- Return a borrowed book
 UPDATE books SET available = TRUE WHERE id = 1;
-UPDATE patrons SET borrowed_books = array_remove(borrowed_books, 1) WHERE id = 1;
+UPDATE patrons SET borrowed_books = array_remove(borrowed_books, 1) WHERE id = 1;"-- Update: change patron membership status" 
